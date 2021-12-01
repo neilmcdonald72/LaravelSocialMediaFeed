84d324d0fbdf3f794ccdcd8a081e07378021445a
Neil McDonald <w0225318@campus.nscc.ca>
2021-11-30 23:50:22 -0400

    step3

---
+++ SMFeed-w0225318/database/migrations/2021_12_01_010229_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('[content]');
+            $table->integer('created_by');
+            $table->integer('deleted_by');
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+            $table->dateTime('deleted_at', 0);
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
